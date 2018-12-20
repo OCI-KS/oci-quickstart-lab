28ae1910f4138cd5fe75ea03d10e187f78c9755d
snafuz <snafuz.and@gmail.com>
2018-12-19 18:09:02 -0800

    setup script

--- quickstart-setup.sh
+++ quickstart-setup.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-yum install httpd mod_wsgi
 
-mkdir /var/tmp/quickstart
-cd /var/tmp/quickstart
+#wget https://github.com/snafuz/oci-quickstart-lab/archive/master.zip
+#unzip master.zip
+#cd oci-quickstart-lab-master
+#sudo quickstart-setup.sh
+
+SRC_DIR=/var/tmp/quickstart
+
+mv ./quickstart.conf /etc/httpd/conf.d/
+mv ./* /var/www/quickstart/
+chown root:root -R /var/www/quickstart
+
+yum install httpd mod_wsgi
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install flask flask-socketio gevent
-#wget ...
-tar -xvzf quickstart-lab.tgz
-mv oci-quickstart-lab/quickstart.conf /etc/httpd/conf.d/
-
-mkdir /var/www/quickstart
-mv oci-quickstart-lab/* /var/www/quickstart/
-
-chown root:root -R /var/www/quickstart
 
 #semanage fcontext -a -t httpd_sys_rw_content_t '/path/to/your/app(/.*)?'
 #restorecon -R -v /var/www/quickstart/
