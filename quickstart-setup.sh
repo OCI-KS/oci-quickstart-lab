bda423b3c24512bc6068766799d664740da52381
snafuz <snafuz.and@gmail.com>
2018-12-19 18:58:15 -0800

    setup script

--- quickstart-setup.sh
+++ quickstart-setup.sh
@@ -6,19 +6,21 @@
 #cd oci-quickstart-lab-master
 #sudo quickstart-setup.sh
 
-SRC_DIR=/var/tmp/quickstart
 
-mv ./quickstart.conf /etc/httpd/conf.d/
+yum install -y httpd mod_wsgi
+
+touch /etc/httpd/conf.d/quickstart.conf
+cat quickstart.conf > /etc/httpd/conf.d/quickstart.conf
+mkdir /var/www/quickstart
 mv ./* /var/www/quickstart/
 chown root:root -R /var/www/quickstart
 
-yum install httpd mod_wsgi
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 pip install flask flask-socketio gevent
 
-#semanage fcontext -a -t httpd_sys_rw_content_t '/path/to/your/app(/.*)?'
-#restorecon -R -v /var/www/quickstart/
+semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/quickstart(/.*)?'
+restorecon -R -v /var/www/quickstart
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --reload
 
